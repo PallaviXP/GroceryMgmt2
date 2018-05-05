24dc8f51de36dcf8bd47bfd034359342e7e6a953
pallavi Dhepe <pallavid@xpanxion.co.in>
2018-05-05 23:18:15 +0530

    Add project files.

---
+++ Backend/GroceryServices/Controllers/GroceryController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroceryServices.Types;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryServices.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Grocery")]
+    public class GroceryController : Controller
+    {
+
+        List<Grocery> _groceryList;
+
+        public GroceryController()
+        {
+            _groceryList = new List<Grocery>() {
+                new Grocery() { Id = 1, Name = "Jaggery", Quantity = 0, Weight = 1 },
+                new Grocery() { Id = 2, Name = "Wheat", Quantity = 0, Weight = 5 },
+                new Grocery() { Id = 3, Name = "Salt", Quantity = 0, Weight = 2 },
+            };
+        }
+
+        // GET: api/Grocery
+        [HttpGet]
+        public IEnumerable<Grocery> Get()
+        {          
+            return _groceryList;
+        }
+
+        // GET: api/Grocery/5
+        [HttpGet("{id}", Name = "Get")]
+        public Grocery Get(int id)
+        {
+            return _groceryList[id-1];
+        }
+        
+        // POST: api/Grocery
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Grocery/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PalGroceryManagement/TodoItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace PalGroceryManagement
+{
+	public class TodoItem
+	{
+		[JsonProperty(PropertyName = "id")]
+		public string Id
+		{
+            get; set;
+		}
+
+		[JsonProperty(PropertyName = "text")]
+		public string Name
+		{
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "quantity")]
+        public int Quantity
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "weight")]
+        public decimal Weight
+        {
+            get; set;
+        }
+
+
+        [JsonProperty(PropertyName = "complete")]
+		public bool Done
+		{
+            get; set;
+        }
+
+        //[Version]
+        //public string Version { get; set; }
+	}
+}
+
