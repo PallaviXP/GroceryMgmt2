ffacb751d30fc428dabb4ba0e08f2a382860eb4b
pallavi Dhepe <pallavid@xpanxion.co.in>
2018-05-06 22:48:13 +0530

    gitignore updated for ignoring keys

--- Backend/GroceryServices/Constants/Keys.cs
+++ Backend/GroceryServices/Constants/Keys.cs
@@ -1,11 +1,14 @@
 ﻿namespace GroceryServices.Constants
 {
-    public class Keys
+    public static class Keys
     {
         //public static readonly string CosmosEndpointUrl = "** URL **";
         public static readonly string CosmosEndpointUrl = "** URL **";
 
         //public static readonly string CosmosAuthKey = "** AUTHKEY **";
         public static readonly string CosmosAuthKey = "** URL **";
+        internal static readonly string DatabaseId = "grocerymanagemnt-database";
+        internal static readonly string CollectionId = "grocerymanagemnt-collection";
+        internal static readonly int OfferThroughput = 400;
     }
 }
--- Backend/GroceryServices/DAL/DBCommunicator.cs
+++ Backend/GroceryServices/DAL/DBCommunicator.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using GroceryServices.Constants;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GroceryServices.DAL
@@ -8,32 +14,119 @@ namespace GroceryServices.DAL
     /// <summary>
     /// Azure CosmosDB - SQLAPI/Document DB API communicator
     /// </summary>
-    public class DBCommunicator<T> : IDBCommunicator<T>
+    public static class DBCommunicator<T> where T : class
     {
-        public async Task<bool> Initialize()
+        private static readonly string DatabaseId = Keys.DatabaseId;
+        private static readonly string CollectionId = Keys.CollectionId;
+        private static DocumentClient _client;
+
+        #region Init
+        public static void Initialize()
         {
-            throw new NotImplementedException();
+            _client = new DocumentClient(new Uri(Keys.CosmosEndpointUrl), Keys.CosmosAuthKey);
+            CreateDatabaseIfNotExistsAsync().Wait();
+            CreateCollectionIfNotExistsAsync().Wait();
         }
 
-        public async Task<List<T>> GetItemsCollection()
+        private static async Task CreateDatabaseIfNotExistsAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(DatabaseId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == HttpStatusCode.NotFound)
+                {
+                    await _client.CreateDatabaseAsync(new Database { Id = DatabaseId });
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
-        public async Task<T> GetItem(int id)
+        private static async Task CreateCollectionIfNotExistsAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await _client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(DatabaseId),
+                        new DocumentCollection
+                        {
+                            Id = CollectionId
+                        },
+                        new RequestOptions { OfferThroughput = Keys.OfferThroughput });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        #endregion
+
+        public static async Task<IEnumerable<T>> GetItemsCollection(Expression<Func<T, bool>> predicate)
+        {
+            IDocumentQuery<T> query = _client.CreateDocumentQuery<T>(
+                UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId),
+                new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true })
+                .Where(predicate)
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
         }
 
-        public async Task SaveItem(T item)
+        public static async Task<T> GetItem(string id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Document document =
+                    await _client.ReadDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+                return (T)(dynamic)document;
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
-        public async Task DeleteItem(int id)
+        public static async Task<Document> CreateItemAsync(T item)
         {
-            throw new NotImplementedException();
+            return await _client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), item);
         }
+
+        public static async Task<Document> UpdateItemAsync(string id, T item)
+        {
+            return await _client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id), item);
+        }
+
+        public static async Task DeleteItemAsync(string id, string category)
+        {
+            await _client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+        }
+
+
 
     }
 }
